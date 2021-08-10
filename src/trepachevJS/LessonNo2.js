60cb7ac2462e06676f8ab2a699e6e202ec2e0747
Egorych <mikhalevichegor@gmail.com>
2021-08-10 22:59:46 +0300

    Задача 2. Напишите скрипт, который считает количество секунд в часе, сутках.

--- src/trepachevJS/LessonNo2.js
+++ src/trepachevJS/LessonNo2.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 export function LessonNo2() {
-    /*    Задача.
+    /*    Задача 1.
      Создайте переменную str и присвойте ей значение 'abcde'.
      Обращаясь к отдельным символам этой строки выведите на экран символ 'a', символ 'b', символ 'e'.
      */
     let str = 'abcde'
-    console.log(str[0])
-    console.log(str[1])
-    console.log(str[2])
-    console.log(str[3])
-    console.log(str[4])
+    console.log(str[0], [1], [2], [3], [4])
 
+    // Задача 2. Напишите скрипт, который считает количество секунд в часе, сутках.
+    let secondClock = 60 * 60
+    let clockDay = 60 * 60 * 24
+    console.log('Количество секунд в часе ' + secondClock)
+    console.log('Количество секунд в сутках ' + clockDay)
     return (
         <div className={"lessonJs"}>
             <h2>Задачи на основы языка JavaScript</h2>
