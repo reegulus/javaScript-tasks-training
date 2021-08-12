51ad724b8016a1782ca10feeb31109a6022e0693
Egorych <mikhalevichegor@gmail.com>
2021-08-12 22:31:02 +0300

    Tasks on the basics of working with arrays and objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {LessonNo2} from './trepachevJS/LessonNo2'
+import {LessonNo3} from "./trepachevJS/LessonNo3";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Задачи языка JavaScript</h1>
-      <LessonNo2/>
+      {/*<LessonNo2/>*/}
+      <LessonNo3/>
 
     </div>
   );
