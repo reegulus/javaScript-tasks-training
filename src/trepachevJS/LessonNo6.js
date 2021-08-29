2cd4b81e621bd032550f03bf9c4f72928eb51824
Egorych <mikhalevichegor@gmail.com>
2021-08-29 17:26:09 +0300

    Задача 1. Поиск и замена

--- src/trepachevJS/LessonNo6.js
+++ src/trepachevJS/LessonNo6.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 export function LessonNo6() {
+
+    //Задача 1. Поиск и замена
+    let str = 'aaa@bbb@ccc';
+    console.log(str.replace('@', '!')); //получим 'aaa!bbb@ccc'
 return (
     <div>
         <h2>Задачи на функции работы со строками в JavaScript</h2>
-        Задача 1. Поиск и замена
+
 
     </div>
 )
