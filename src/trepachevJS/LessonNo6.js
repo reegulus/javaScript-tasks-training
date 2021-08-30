a21bbfdf6b47e4ad497b893d2e876e3f42b99d63
Egorych <mikhalevichegor@gmail.com>
2021-08-30 20:34:59 +0300

    Методы substr, substring, slice

--- src/trepachevJS/LessonNo6.js
+++ src/trepachevJS/LessonNo6.js
@@ -3,8 +3,14 @@ import React from "react";
 export function LessonNo6() {
 
     //Задача 1. Поиск и замена
-    let str = 'aaa@bbb@ccc';
-    console.log(str.replace('@', '!')); //получим 'aaa!bbb@ccc'
+  /*  let str = 'aaa@bbb@ccc';
+    console.log(str.replace('@', '!')); //получим 'aaa!bbb@ccc'*/
+
+    // Задача 2 . Методы substr, substring, slice
+    var str = 'aaa bbb ccc';
+    alert(str.substr(4, 3)); //substr(откуда отрезать, сколько отрезать)
+    alert(str.substring(4, 7)); //substring(откуда отрезать, докуда отрезать)
+    alert(str.slice(4, 7)); //slice(откуда отрезать, докуда отрезать)
 return (
     <div>
         <h2>Задачи на функции работы со строками в JavaScript</h2>
